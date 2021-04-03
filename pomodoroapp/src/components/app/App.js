097282eb5b9c4f87853fcc53ea3ad5f74f617f41
Oscar Sotorrio <ososotorrio@hotmail.com>
2021-04-03 12:46:39 +0200

    Added boostrap library

--- pomodoroapp/src/components/app/App.js
+++ pomodoroapp/src/components/app/App.js
@@ -2,8 +2,24 @@ import Todo from '../todo/todo';
 import './App.css';
 
 function App() {
+  const border = {
+    border: '1px solid black' 
+  }
+
   return (
-    <Todo />
+    <div className="container">
+      <div className="row">
+        <div className="col-6" style={border}>
+          <h1>This is our timer component</h1>
+        </div>
+        <div className="col-6" style={border}>
+          <Todo />
+        </div>
+      </div>
+      <div className="row" style={border}>
+        <h1>This is our history component</h1>
+      </div>
+    </div>
   );
 }
 
--- pomodoroapp/src/index.js
+++ pomodoroapp/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
