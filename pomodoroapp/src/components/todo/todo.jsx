8c7bd50e82ad1bd26daf3e5f4e60d5274b685d1e
Oscar Sotorrio <ososotorrio@hotmail.com>
2021-04-03 13:01:04 +0200

    Added boostrap form

--- pomodoroapp/src/components/todo/todo.jsx
+++ pomodoroapp/src/components/todo/todo.jsx
@@ -31,6 +31,22 @@ class Todo extends React.Component {
     render(){
         return (
             <div>
+                <form>
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                    <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com" />
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                    <input type="password" class="form-control" id="inputPassword" placeholder="Password" />
+                    </div>
+                </div>
+                </form>
+
+                
                 TODO LIST
                 <form onSubmit={this.handleSubmit}>
                     <input type="text" value={this.state.currentText} onChange={this.handleChange} required />
@@ -41,7 +57,7 @@ class Todo extends React.Component {
                         <li key={index}>
                             <input type="checkbox"/>
                             <span>{todo}</span>   
-                            <span id={index} onClick={this.handleClick}>X</span>
+                            <button id={index} onClick={this.handleClick}>X</button>
                         </li> 
                     ))
                     }
