e205d650ecc6133577388175523a3ea4b78ed5e8
Fernando Ortega Martinez <fernando.martinez@just-eat.com>
2021-04-03 11:00:10 +0100

    work in progress with todo component

--- pomodoroapp/src/components/todo/todo.jsx
+++ pomodoroapp/src/components/todo/todo.jsx
@@ -3,17 +3,50 @@ import React from 'react';
 
 class Todo extends React.Component {
 
+
+    constructor(){
+        super()
+
+        this.state = {
+            currentText: "",
+            todos: []
+        }
+    }
+   
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        
+        this.setState((previousState) => ({
+            currentText: "",
+            todos: previousState.todos.concat(previousState.currentText)
+        }))
+        
+        console.log(this.state.currentText)
+
+    }
+
+    handleChange = (event) => {
+        this.setState({currentText:event.target.value})
+    }
+
     render(){
         return (
             <div>
                 TODO LIST
-                <input type="text"/>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange} required />
+                </form>
                 <ul>
-                   <li>
-                    <input type="checkbox"/>
-                    <span>bla bla bla</span>   
-                    <span>X</span>
-                    </li> 
+                    {
+                    this.state.todos.map((todo,index) => (
+                        <li key={index}>
+                            <input type="checkbox"/>
+                            <span>{todo}</span>   
+                            <span>X</span>
+                        </li> 
+                    ))
+                    }
                 </ul>
             </div>
         )
